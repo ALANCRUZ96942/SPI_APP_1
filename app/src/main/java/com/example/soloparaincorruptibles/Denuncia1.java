af9bf73603655cbf665dca6a83861d047bf9b8bc
Alan Cruz <alan96942@gmail.com>
2020-10-29 15:06:06 -0600

    actualización denuncias

--- app/src/main/java/com/example/soloparaincorruptibles/Denuncia1.java
+++ app/src/main/java/com/example/soloparaincorruptibles/Denuncia1.java
@@ -2,28 +2,50 @@ package com.example.soloparaincorruptibles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.TimePicker;
 
 public class Denuncia1 extends AppCompatActivity {
 
+    private Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_denuncia1);
+        Button botonSiguiente =(Button)findViewById(R.id.siguiente0);
 
-        Button opciona = findViewById(R.id.opciona);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColor(R.color.primero)));
+        this.window = getWindow();
+        window.setStatusBarColor(getColor(R.color.oscuro));
+        window.setNavigationBarColor(getColor(R.color.oscuro));
 
-        opciona.setOnClickListener(new View.OnClickListener() {
+
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent5 = new Intent(v.getContext(), MainActivity.class);
-                startActivityForResult(intent5, 0);
+                Intent intentA = new Intent(v.getContext(), Denuncias.class);
+                startActivityForResult(intentA, 0);
             }
         });
 
 
+    }
+
+
+    public void abrirAyuda(View view) {
+        Dialog settingsDialog = new Dialog(this);
+        settingsDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        settingsDialog.setContentView(getLayoutInflater().inflate(R.layout.image_layout, null));
+        settingsDialog.show();
     }
 }
--- app/src/main/java/com/example/soloparaincorruptibles/Denuncias6.java
+++ app/src/main/java/com/example/soloparaincorruptibles/Denuncias6.java
@@ -3,33 +3,36 @@ package com.example.soloparaincorruptibles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
-public class Denuncias extends AppCompatActivity {
-
+public class Denuncias6 extends AppCompatActivity {
     private Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_denuncias);
-        Button botonSiguiente =(Button)findViewById(R.id.siguiente1);
+        setContentView(R.layout.activity_denuncias6);
 
         this.window = getWindow();
         window.setStatusBarColor(getColor(R.color.oscuro));
         window.setNavigationBarColor(getColor(R.color.oscuro));
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColor(R.color.primero)));
+        Button siguiente = (Button)findViewById(R.id.siguiente0);
+        siguiente.setOnClickListener(new View.OnClickListener() {
 
-        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intentA = new Intent(v.getContext(), Denuncias3.class);
+                Intent intentA = new Intent(v.getContext(), Denuncias7.class);
                 startActivityForResult(intentA, 0);
             }
+
+
+
+
         });
 
     }
